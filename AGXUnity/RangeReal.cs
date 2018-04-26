b151814b8afd0ec7d287465738ba04121d7622f4
Niklas Melin <niklas.melin@algoryx.se>
2018-04-26 22:02:45 +0200

    Added RangeReal single value constructor.

--- AGXUnity/RangeReal.cs
+++ AGXUnity/RangeReal.cs
@@ -41,6 +41,16 @@
     }
 
     /// <summary>
+    /// Construct given value of negative and positive bound, i.e.,
+    /// range = (-value, value).
+    /// </summary>
+    /// <param name="value">Negative and positive value of the range.</param>
+    public RangeReal( float value )
+      : this( -UnityEngine.Mathf.Abs( value ), UnityEngine.Mathf.Abs( value ) )
+    {
+    }
+
+    /// <summary>
     /// Copy constructor.
     /// </summary>
     public RangeReal( RangeReal other )
