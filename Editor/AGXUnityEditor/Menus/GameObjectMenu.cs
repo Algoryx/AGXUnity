449554a9dd8578484c80c8dc3c56eaf5d43d0f13
Niklas Melin <niklas@algoryx.se>
2019-05-02 12:05:43 +0200

    Fix so that Create Shape Visual works when multiple objects are selected.

--- Editor/AGXUnityEditor/Menus/GameObjectMenu.cs
+++ Editor/AGXUnityEditor/Menus/GameObjectMenu.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using AGXUnity.Rendering;
@@ -11,55 +13,59 @@ namespace AGXUnityEditor.Menus
     [MenuItem( "GameObject/AGX Unity/Create Shape Visual in children", false, 0 )]
     public static void CreateVisual()
     {
-      if ( Selection.activeGameObject == null ) {
-        Debug.Log( "Unable to create visual: No game object selected in scene." );
+      // Multi-selection and we're receiving one call per selected object
+      // but it's not possible to process one object each call when
+      // Selection.active* aren't changed - so we're blocking calls for
+      // x seconds.
+      if ( EditorApplication.timeSinceStartup - m_lastCreateVisualCall < 0.5 )
+        return;
+
+      var objects = Selection.GetFiltered<GameObject>( SelectionMode.Editable | SelectionMode.TopLevel );
+      if ( objects.Length == 0 ) {
+        Debug.Log( "Unable to create visual: Selected objects not supported (not editable - such as prefabs)." );
         return;
       }
 
-      if ( AssetDatabase.Contains( Selection.activeGameObject ) ) {
-        Debug.Log( "Unable to create visual: Selected game object is not an instance in a scene.", Selection.activeGameObject );
-        return;
-      }
+      var validShapes            = from go in objects
+                                   from shape in go.GetComponentsInChildren<Shape>()
+                                   where !ShapeVisual.HasShapeVisual( shape ) &&
+                                          ShapeVisual.SupportsShapeVisual( shape )
+                                   select shape;
+      var numSensors             = validShapes.Count( shape => shape.IsSensor );
+      var createVisualForSensors = validShapes.Count() > 0 &&
+                                   numSensors > 0 &&
+                                   // Negated when "No" is first (left button)
+                                   !EditorUtility.DisplayDialog( "Sensors",
+                                                                 "There are " +
+                                                                   numSensors +
+                                                                   " sensors in this object. Would you like to" +
+                                                                   " create visuals for sensors as well?",
+                                                                 "No",
+                                                                 "Yes" );
 
-      var shapes = Selection.activeGameObject.GetComponentsInChildren<Shape>();
-      if ( shapes.Length == 0 ) {
-        Debug.Log( "Unable to create visual: Selected game object doesn't have any shapes.", Selection.activeGameObject );
-        return;
-      }
+      if ( validShapes.Count() > 0 && ( createVisualForSensors || numSensors < validShapes.Count() ) ) {
+        Undo.SetCurrentGroupName( "Create GameObject(s) shape visual." );
+        var grouId = Undo.GetCurrentGroup();
 
-      int numValidCreateVisual = 0;
-      int numSensors = 0;
-      foreach ( var shape in shapes ) {
-        var validForVisual    = !ShapeVisual.HasShapeVisual( shape ) && ShapeVisual.SupportsShapeVisual( shape );
-        numValidCreateVisual += Convert.ToInt32( validForVisual );
-        numSensors           += Convert.ToInt32( validForVisual && shape.IsSensor );
-      }
+        foreach ( var shape in validShapes ) {
+          if ( shape.IsSensor && !createVisualForSensors )
+            continue;
 
-      var createVisualForSensors = false;
-      if ( numSensors > 0 )
-        createVisualForSensors = EditorUtility.DisplayDialog( "Sensors",
-                                                              "There are " + numSensors + " sensors in this object. Would you like to create visuals for sensors as well?",
-                                                              "Yes",
-                                                              "No" );
-
-      if ( numValidCreateVisual == 0 || numSensors >= numValidCreateVisual ) {
-        Debug.Log( "Create visual ignored: All shapes already have visual data or doesn't support to be visualized.", Selection.activeGameObject );
-        return;
-      }
-
-      Undo.SetCurrentGroupName( "Create GameObject shape visual." );
-      var grouId = Undo.GetCurrentGroup();
-      foreach ( var shape in shapes ) {
-        var createVisual = !ShapeVisual.HasShapeVisual( shape ) &&
-                           ( createVisualForSensors || !shape.IsSensor );
-        if ( createVisual ) {
-          var go = ShapeVisual.Create( shape );
-          if ( go != null )
-            Undo.RegisterCreatedObjectUndo( go, "Shape visual" );
+          var visual = ShapeVisual.Create( shape );
+          if ( visual != null )
+            Undo.RegisterCreatedObjectUndo( visual, "Create visual: " + shape.name );
         }
+
+        Undo.CollapseUndoOperations( grouId );
+      }
+      else {
+        Debug.Log( "Create visual ignored: All shapes already have visual data or doesn't support to be visualized.",
+                   Selection.activeGameObject );
       }
 
-      Undo.CollapseUndoOperations( grouId );
+      m_lastCreateVisualCall = EditorApplication.timeSinceStartup;
     }
+
+    private static double m_lastCreateVisualCall = 0.0;
   }
 }
