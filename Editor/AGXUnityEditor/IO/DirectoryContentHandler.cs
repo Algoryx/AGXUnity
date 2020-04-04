24384cf796399fcbd838ceb08e069b7f7b074cfd
Niklas Melin <niklas.melin@algoryx.se>
2020-04-04 23:55:49 +0200

    Removing all current content post restart, during update, before importing the updated package.

---
+++ Editor/AGXUnityEditor/IO/DirectoryContentHandler.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using UnityEngine;
+using UnityEditor;
+
+namespace AGXUnityEditor.IO
+{
+  public class DirectoryContentHandler
+  {
+    /// <summary>
+    /// Delete all content that's not user specific or possibly loaded
+    /// by the editor (dll files).
+    /// </summary>
+    /// <returns>True if no exception - otherwise false.</returns>
+    public static bool DeleteContent()
+    {
+      try {
+        var handler = new DirectoryContentHandler( Utils.AGXUnityPackageDirectory );
+        handler.DeleteAllCollected();
+      }
+      catch ( System.Exception ) {
+        return false;
+      }
+
+      return true;
+    }
+
+    /// <summary>
+    /// Root directory to parse files and directories from.
+    /// </summary>
+    public DirectoryInfo RootDirectory { get; private set; } = null;
+
+    /// <summary>
+    /// All files from RootDirectory that may be deleted.
+    /// </summary>
+    public FileInfo[] Files { get { return m_files.ToArray(); } }
+
+    /// <summary>
+    /// Files that are ignored in the files to may be deleted collection.
+    /// </summary>
+    public FileInfo[] IgnoredFiles { get { return m_ignoredFiles.ToArray(); } }
+
+    /// <summary>
+    /// Directories that may be deleted due to empty contents.
+    /// </summary>
+    public DirectoryInfo[] Directories
+    {
+      get
+      {
+        return ( from directory in m_directories.ToList()
+                 where MayDelete( directory )
+                 select directory ).ToArray();
+      }
+    }
+
+    /// <summary>
+    /// All directories with collected files.
+    /// </summary>
+    public DirectoryInfo[] AllDirectories { get { return m_directories.ToArray(); } }
+
+    public DirectoryContentHandler( string rootDirectory )
+      : this( new DirectoryInfo( rootDirectory ) )
+    {
+    }
+
+    public DirectoryContentHandler( DirectoryInfo rootDirectory )
+    {
+      if ( rootDirectory == null )
+        throw new System.ArgumentNullException( "rootDirectory" );
+      if ( !rootDirectory.Exists )
+        throw new DirectoryNotFoundException( rootDirectory.FullName + " doesn't exists." );
+
+      RootDirectory = rootDirectory;
+
+      CollectData( RootDirectory );
+    }
+
+    public void DeleteAllCollected()
+    {
+      EditorApplication.LockReloadAssemblies();
+
+      Debug.Log( "    - Ignoring delete of files:" );
+      foreach ( var file in m_ignoredFiles )
+        Debug.Log( $"       {file.FullName}" );
+
+      Debug.Log( $"   - Deleting {m_files.Count} files..." );
+      foreach ( var file in m_files ) {
+        try {
+          file.Delete();
+        }
+        catch ( System.Exception e ) {
+          Debug.LogError( $"        Unable to remove file: {file.FullName}\n                {e.Message}" );
+        }
+      }
+
+      var directories = Directories;
+      Debug.Log( $"    - Deleting {directories.Length} directories..." );
+      foreach ( var directory in directories ) {
+        if ( MayDelete( directory ) ) {
+          try {
+            directory.Delete();
+          }
+          catch ( System.Exception ) {
+            Debug.LogError( $"        Unable to remove directory: {directory.FullName}\nIs it open in File Explorer or cmd?" );
+          }
+        }
+      }
+
+      EditorApplication.UnlockReloadAssemblies();
+    }
+
+    private void CollectData( DirectoryInfo currentDirectory )
+    {
+      try {
+        var files = currentDirectory.GetFiles( "*.*" );
+        foreach ( var file in files ) {
+          if ( MayDelete( file ) )
+            m_files.Add( file );
+          else
+            AddIgnoredFile( file );
+        }
+
+        foreach ( var directory in currentDirectory.GetDirectories() ) {
+          m_directories.Add( directory );
+          CollectData( directory );
+        }
+      }
+      catch ( System.UnauthorizedAccessException ) {
+      }
+      catch ( DirectoryNotFoundException ) {
+      }
+      catch ( System.Exception e ) {
+        Debug.LogException( e );
+      }
+    }
+
+    private bool MayDelete( FileInfo file )
+    {
+      var ignoreFile = file.Name.Contains( "agx.lic" ) ||
+                       ( file.Name.Contains( "Data.asset" ) && file.Directory.Name == "Data" ) ||
+                       ( file.Name.Contains( "Settings.asset" ) && file.Directory.Name == "Data" ) ||
+                       ( file.Extension == ".dll" || file.Name.EndsWith( ".dll.meta" ) );
+      return !ignoreFile;
+    }
+
+    private bool MayDelete( DirectoryInfo directory )
+    {
+      return !m_ignoreDirectories.Contains( directory );
+    }
+
+    private void AddIgnoredFile( FileInfo file )
+    {
+      m_ignoredFiles.Add( file );
+      m_ignoreDirectories.Add( file.Directory );
+      var parent = file.Directory.Parent;
+      while ( parent != null && parent.FullName != RootDirectory.FullName ) {
+        m_ignoreDirectories.Add( parent );
+        parent = parent.Parent;
+      }
+    }
+
+    class DistinctFileSystemInfoComparer : IEqualityComparer<FileSystemInfo>
+    {
+      public bool Equals( FileSystemInfo x, FileSystemInfo y )
+      {
+        return x.FullName == y.FullName;
+      }
+
+      public int GetHashCode( FileSystemInfo obj )
+      {
+        return obj.FullName.GetHashCode();
+      }
+    }
+
+    class DirectoryFullNameMaxLengthComparer : IComparer<DirectoryInfo>
+    {
+      public int Compare( DirectoryInfo x, DirectoryInfo y )
+      {
+        return x.FullName.Length < y.FullName.Length ? 1 : x.FullName.Length > y.FullName.Length ? -1 : 0;
+      }
+    }
+
+    private List<FileInfo> m_files = new List<FileInfo>();
+    private HashSet<FileInfo> m_ignoredFiles = new HashSet<FileInfo>( new DistinctFileSystemInfoComparer() );
+    private List<DirectoryInfo> m_directories = new List<DirectoryInfo>();
+    private HashSet<DirectoryInfo> m_ignoreDirectories = new HashSet<DirectoryInfo>( new DistinctFileSystemInfoComparer() );
+  }
+}
--- Editor/AGXUnityEditor/PackageUpdateHandler.cs
+++ Editor/AGXUnityEditor/PackageUpdateHandler.cs
@@ -27,9 +27,18 @@ namespace AGXUnityEditor
         return;
       }
 
+      if ( !EditorUtility.DisplayDialog( "AGX Dynamics for Unity update",
+                                         $"AGX Dynamics for Unit is about to be updated, make sure all\n" +
+                                         $"File Explorer and/or terminals in {IO.Utils.AGXUnityPackageDirectory}\n" +
+                                         $"are closed during this process. Are all explorers and terminals\n" +
+                                         $"closed and ready for install?",
+                                         "Yes",
+                                         "No" ) )
+        return;
+
       var unsavedScenes = ( from scene in Scenes where scene.isDirty select scene ).ToArray();
       if ( unsavedScenes.Length > 0 ) {
-        var saveSceneInstruction = "AGX Dynamics for Unity (AGXUnity) is about to be updated. " +
+        var saveSceneInstruction = "AGX Dynamics for Unity is about to be updated. " +
                                    "A new empty scene is required for AGXUnity to update. " +
                                    "The following scenes aren't saved:\n";
         foreach ( var scene in unsavedScenes )
@@ -96,11 +105,22 @@ namespace AGXUnityEditor
         Debug.Log( "Removing old native binaries..." );
         foreach ( var nativePlugin in NativePlugins ) {
           var fi = new FileInfo( nativePlugin.assetPath );
+          Debug.Log( $"    - {fi.Name}" );
           var fiMeta = new FileInfo( nativePlugin.assetPath + ".meta" );
-          fi.Delete();
-          if ( fiMeta.Exists )
-            fiMeta.Delete();
+          try {
+            fi.Delete();
+            if ( fiMeta.Exists )
+              fiMeta.Delete();
+          }
+          catch ( System.Exception e ) {
+            Debug.LogException( e );
+            Debug.LogError( "Fatal update error: Close Unity and remove AGX Dynamics for Unity directory and install the latest version." );
+            return;
+          }
         }
+
+        Debug.Log( "Removing all non-user specific content..." );
+        IO.DirectoryContentHandler.DeleteContent();
 
         // TODO: Remove all files to handle rename/remove of scripts and content.
         //       1. Create a new empty scene (save dialog handled pre-restart)
@@ -150,5 +170,78 @@ namespace AGXUnityEditor
     }
 
     private static readonly string s_packageIdentifier = "package_name:";
+  }
+
+  public class PackageContentHandler
+  {
+    [MenuItem( "Test/Collect Files" )]
+    private static void DoIt()
+    {
+      ( new PackageContentHandler() ).CollectData();
+    }
+
+    public void CollectData()
+    {
+      CollectData( new DirectoryInfo( IO.Utils.AGXUnityPackageDirectory ) );
+
+      Debug.Log( $"Files collected: {m_files.Count}, Directory collected: {m_directories.Count}" );
+      foreach ( var ignoredDirectory in m_ignoreDirectories )
+        Debug.Log( $"Won't delete directory: {ignoredDirectory.FullName}" );
+
+      Debug.Log( m_ignoreDirectories.Contains( new DirectoryInfo( @"D:\Unity\AGXDynamicsForUnity_2019.3\Assets\AGXUnity\Editor\Data" ) ) );
+    }
+
+    public void CollectData( DirectoryInfo currentDirectory )
+    {
+      try {
+        var files = currentDirectory.GetFiles( "*.*" );
+        foreach ( var file in files ) {
+          if ( MayDelete( file ) )
+            m_files.Add( file );
+          else {
+            m_ignoredFiles.Add( file );
+            m_ignoreDirectories.Add( file.Directory );
+          }
+        }
+
+        foreach ( var directory in currentDirectory.GetDirectories() ) {
+          m_directories.Add( directory );
+          CollectData( directory );
+        }
+      }
+      catch ( System.UnauthorizedAccessException ) {
+      }
+      catch ( DirectoryNotFoundException ) {
+      }
+      catch ( System.Exception e ) {
+        Debug.LogException( e );
+      }
+    }
+
+    private bool MayDelete( FileInfo fi )
+    {
+      var ignoreFile = fi.Name.Contains( "agx.lic" ) ||
+                       ( fi.Name.Contains( "Data.asset" ) && fi.Directory.Name == "Data" ) ||
+                       ( fi.Name.Contains( "Settings.asset" ) && fi.Directory.Name == "Data" );
+      return !ignoreFile;
+    }
+
+    class DistinctFileSystemInfoComparer : IEqualityComparer<FileSystemInfo>
+    {
+      public bool Equals( FileSystemInfo x, FileSystemInfo y )
+      {
+        return x.FullName == y.FullName;
+      }
+
+      public int GetHashCode( FileSystemInfo obj )
+      {
+        return obj.FullName.GetHashCode();
+      }
+    }
+
+    private List<FileInfo> m_files = new List<FileInfo>();
+    private HashSet<FileInfo> m_ignoredFiles = new HashSet<FileInfo>( new DistinctFileSystemInfoComparer() );
+    private List<DirectoryInfo> m_directories = new List<DirectoryInfo>();
+    private HashSet<DirectoryInfo> m_ignoreDirectories = new HashSet<DirectoryInfo>( new DistinctFileSystemInfoComparer() );
   }
 }
