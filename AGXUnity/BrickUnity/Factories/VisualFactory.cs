8c1541beffbf019f7ab5bedf2e010fd2b53e169b
Sofi Backman <sofi.backman@algoryx.se>
2021-03-29 16:38:05 +0200

    Remove unnecessary imports

--- AGXUnity/BrickUnity/Factories/VisualFactory.cs
+++ AGXUnity/BrickUnity/Factories/VisualFactory.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityMeshImporter;
-using AGXUnity.Utils;
 
 using B_VisualShape = Brick.Visual.Shape;
 
