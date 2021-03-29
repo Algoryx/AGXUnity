67590ec67373c6725bdafd92ce49a5ec284d8aca
Joakim Mörk <joakim.mork@algoryx.se>
2021-03-29 10:16:31 +0200

    Render granules with DrawMeshInstanced

--- AGXUnity/Rendering/DeformableTerrainParticleRenderer.cs
+++ AGXUnity/Rendering/DeformableTerrainParticleRenderer.cs
@@ -8,11 +8,42 @@ namespace AGXUnity.Rendering
   [RequireComponent( typeof( DeformableTerrain ) )]
   public class DeformableTerrainParticleRenderer : ScriptComponent
   {
+    public enum GranuleRenderMode
+    {
+      GameObject = 0,
+      DrawMeshInstanced = 1
+    }
+
     [HideInInspector]
     public DeformableTerrain DeformableTerrain { get; private set; } = null;
 
+    [Tooltip("Render particles using cloned GameObjects or with Graphics.DrawMeshInstanced")]
+    [SerializeField]
+    private GranuleRenderMode m_renderMode = GranuleRenderMode.GameObject;
+
     [SerializeField]
     private GameObject m_granuleInstance = null;
+    
+    public Mesh GranuleDrawInstancedMesh = null;
+    public Material GranuleDrawInstancedMaterial = null;
+
+    private Matrix4x4[] m_granuleMatrices;
+    private int m_numGranulars = 0;
+    private MaterialPropertyBlock m_properties = null;
+
+    public GranuleRenderMode RenderMode
+    {
+      get { return m_renderMode; }
+      set
+      {
+        if (m_renderMode == GranuleRenderMode.GameObject && value != GranuleRenderMode.GameObject)
+        {
+          Destroy(m_numGranulars);
+        }
+
+        m_renderMode = value;
+      }
+    }
 
     public GameObject GranuleInstance
     {
@@ -39,6 +70,10 @@ namespace AGXUnity.Rendering
       DeformableTerrain = GetComponent<DeformableTerrain>();
       if ( DeformableTerrain == null )
         return false;
+
+      m_granuleMatrices = new Matrix4x4[1023];
+
+      m_properties = new MaterialPropertyBlock();
 
       Synchronize();
 
@@ -74,37 +109,83 @@ namespace AGXUnity.Rendering
       Synchronize();
     }
 
+    private void Update()
+    {
+      if (RenderMode == GranuleRenderMode.DrawMeshInstanced && m_numGranulars > 0)
+      {
+        if (m_numGranulars < 1024)
+          Graphics.DrawMeshInstanced(GranuleDrawInstancedMesh, 0, GranuleDrawInstancedMaterial, m_granuleMatrices, m_numGranulars, m_properties, UnityEngine.Rendering.ShadowCastingMode.On, true);
+        else // DrawMeshInstanced only supports up to 1023 meshes for each call, we need to subdivide if we have more particles than that
+        { 
+          for (int i = 0; i < m_numGranulars; i += 1023)
+          {
+            int count = Mathf.Min(1023, m_numGranulars - i);
+            Graphics.DrawMeshInstanced(GranuleDrawInstancedMesh, 0, GranuleDrawInstancedMaterial, new System.ArraySegment<Matrix4x4>(m_granuleMatrices, i, count).Array, count, m_properties, UnityEngine.Rendering.ShadowCastingMode.On, true);
+          }
+        }
+      }
+    }
+
     private void Synchronize()
     {
-      if ( GranuleInstance == null || DeformableTerrain == null || DeformableTerrain.Native == null )
+      if ( DeformableTerrain == null || DeformableTerrain.Native == null )
         return;
 
       var soilSimulation = DeformableTerrain.Native.getSoilSimulationInterface();
       var granulars      = soilSimulation.getSoilParticles();
-      var numGranulars   = (int)granulars.size();
+      m_numGranulars   = (int)granulars.size();
 
-      // More granular instances comparing to last time, create
-      // more instances to match numGranulars.
-      if ( numGranulars > transform.childCount )
-        Create( numGranulars - transform.childCount );
-      // Less granular instances comparing to last time, destroy.
-      else if ( transform.childCount > numGranulars )
-        Destroy( transform.childCount - numGranulars );
+      if (RenderMode == GranuleRenderMode.DrawMeshInstanced)
+      {
+        if (GranuleDrawInstancedMaterial == null || GranuleDrawInstancedMesh == null)
+          return;
 
-      Debug.Assert( transform.childCount == numGranulars );
+        // Use 1023 as arbitrary block size since that is the amount of particles that can be drawn with DrawMeshInstanced
+        if (m_numGranulars > m_granuleMatrices.Length)
+          System.Array.Resize(ref m_granuleMatrices, (m_numGranulars / 1023 + 1) * 1023);
 
-      for ( int i = 0; i < numGranulars; ++i ) {
-        var granule  = granulars.at( (uint)i );
-        var instance = transform.GetChild( i );
-        instance.position = granule.position().ToHandedVector3();
-        instance.rotation = granule.rotation().ToHandedQuaternion();
+        for (int i = 0; i < m_numGranulars; i++)
+        {
+          var granule = granulars.at((uint)i);
 
-        // Assuming unit size of the instance, scale to diameter
-        // of the granule.
-        instance.localScale = Vector3.one * 2.0f * (float)granule.getRadius();
+          m_granuleMatrices[i] = Matrix4x4.TRS(
+            granule.position().ToHandedVector3(),
+            granule.rotation().ToHandedQuaternion(),
+            Vector3.one * 2.0f * (float)granule.getRadius()); // Assuming unit size of the instance, scale to diameter of the granule.
 
-        // Return the proxy class to the pool to avoid garbage.
-        granule.ReturnToPool();
+          // Return the proxy class to the pool to avoid garbage.
+          granule.ReturnToPool();
+        }
+      }
+      else if (RenderMode == GranuleRenderMode.GameObject)
+      {
+        if (GranuleInstance == null)
+          return;
+
+        // More granular instances comparing to last time, create
+        // more instances to match numGranulars.
+        if (m_numGranulars > transform.childCount)
+          Create(m_numGranulars - transform.childCount);
+        // Less granular instances comparing to last time, destroy.
+        else if (transform.childCount > m_numGranulars)
+          Destroy(transform.childCount - m_numGranulars);
+
+        Debug.Assert(transform.childCount == m_numGranulars);
+
+        for (int i = 0; i < m_numGranulars; ++i)
+        {
+          var granule = granulars.at((uint)i);
+          var instance = transform.GetChild(i);
+          instance.position = granule.position().ToHandedVector3();
+          instance.rotation = granule.rotation().ToHandedQuaternion();
+
+          // Assuming unit size of the instance, scale to diameter
+          // of the granule.
+          instance.localScale = Vector3.one * 2.0f * (float)granule.getRadius();
+
+          // Return the proxy class to the pool to avoid garbage.
+          granule.ReturnToPool();
+        }
       }
     }
 
