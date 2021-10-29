e294675f43a120d69f9a73a019d43109745763c9
Niklas Melin <niklas.melin@algoryx.se>
2021-10-29 09:52:06 +0200

    UniqueGameObject API documentation and some support for multiple instances during adaptive scene loads/unloads. It's still quite undefined to have multiple instances since Instance will always return the first created instance.

--- AGXUnity/UniqueGameObject.cs
+++ AGXUnity/UniqueGameObject.cs
@@ -6,109 +6,19 @@ using Scene = UnityEngine.SceneManagement.Scene;
 namespace AGXUnity
 {
   /// <summary>
-  /// Singleton like object that is created when Instance is called
-  /// AND this object hasn't been destroyed in the Unity Engine.
-  /// 
-  /// This means that there can only be one instance of this type in
-  /// a scene and that it is unsafe to use this object in the OnDestroy
-  /// callbacks.
-  /// 
-  /// If this object has been destroyed, Instance will return null!
+  /// Singleton like object that is created when Instance is called.
+  /// It's important that callers to Instance properly checks
+  /// HasInstance and/or IsDestroyed when the caller is being
+  /// destroyed, e.g., during scene unload or from OnDestroy.
   /// </summary>
   /// <typeparam name="T">Type of subclass.</typeparam>
   public class UniqueGameObject<T> : ScriptComponent
     where T : ScriptComponent
   {
-#if OLD_SINGLETON
-    protected static T m_instance     = null;
-    protected static bool m_destroyed = false;
-
     /// <summary>
-    /// Returns an instance of this object if it hasn't been destroyed
-    /// in the current context (Unity scene).
+    /// Get found instance or finds instance in the current scene or
+    /// creates a new instance in the scene.
     /// </summary>
-    /// <remarks>
-    /// Note that this property may return null, and if it does, one
-    /// probably wouldn't need the object anyway.
-    /// </remarks>
-    public static T Instance
-    {
-      get
-      {
-        if ( m_destroyed && !Application.isPlaying )
-          ResetDestroyedState();
-
-        var wasNull = m_instance == null;
-
-        if ( !m_destroyed && m_instance == null && ( m_instance = FindObjectOfType( typeof( T ) ) as T ) == null ) {
-          string name = ( typeof( T ).Namespace != null ? typeof( T ).Namespace + "." : "" ) + typeof( T ).Name;
-          m_instance = ( new GameObject( name ) ).AddComponent<T>();
-          m_instance.transform.hideFlags = HideFlags.NotEditable;
-        }
-
-        // When a scene has been unloaded it's safe to create
-        // an instance of this singleton again.
-        if ( wasNull && m_instance != null )
-          SceneManager.sceneUnloaded += OnSceneUnloaded;
-
-        return m_instance;
-      }
-    }
-
-    // (FindObjectOfType( typeof( T ) ) as T ) != null
-    public static bool HasInstance => m_instance != null;
-
-    public static bool InstanceFound => m_instance != null || ( FindObjectOfType( typeof( T ) ) as T ) != null;
-
-    public static bool IsDestroyed { get { return m_destroyed; } }
-
-    /// <summary>
-    /// Use with care. Reset the internal reset state so this
-    /// object may be created again.
-    /// 
-    /// Only call/use this method when you are in the editor!
-    /// </summary>
-    public static void ResetDestroyedState()
-    {
-      m_destroyed = false;
-    }
-
-    protected virtual bool OnInitialize() => true;
-
-    protected override void OnAwake()
-    {
-      m_destroyed = false;
-
-      base.OnAwake();
-    }
-
-    protected sealed override bool Initialize()
-    {
-      // TODO: This isn't the right way to do it. Refactor this class!
-      // https://gamedev.stackexchange.com/questions/116009/in-unity-how-do-i-correctly-implement-the-singleton-pattern
-      int WARNING_READ_TODO;
-      if ( !OnInitialize() )
-        return false;
-
-      if ( m_instance == null )
-        m_instance = this as T;
-
-      return true;
-    }
-
-    protected override void OnDestroy()
-    {
-      m_destroyed = true;
-
-      base.OnDestroy();
-    }
-
-    private static void OnSceneUnloaded( Scene scene )
-    {
-      ResetDestroyedState();
-      SceneManager.sceneUnloaded -= OnSceneUnloaded;
-    }
-#else
     public static T Instance
     {
       get
@@ -122,10 +32,26 @@ namespace AGXUnity
       }
     }
 
+    /// <summary>
+    /// True if the instance has been found or been created.
+    /// False if destroyed or no one has called Instance.
+    /// </summary>
     public static bool HasInstance => s_instance != null;
 
+    /// <summary>
+    /// True if this object has been destroyed. Calling Instance
+    /// after this will create a new instance so if IsDestroyed == true,
+    /// don't call Instance from OnDestroy.
+    /// </summary>
     public static bool IsDestroyed => s_wasCreated && s_instance == null;
 
+    /// <summary>
+    /// True if an instance has been created or there is an instance
+    /// in the loaded scenes. This is used by implicit dependent scripts
+    /// on certain managers, e.g., WindAndWaterParameters shouldn't
+    /// instantiate a dependent WindAndWaterManager if the user hasn't
+    /// explicitly enabled a WindAndWaterManager in a loaded scene.
+    /// </summary>
     public static bool HasInstanceInScene
     {
       get
@@ -136,31 +62,52 @@ namespace AGXUnity
 
     protected sealed override void OnAwake()
     {
-      Instance = this as T;
+      // In adaptive scene loading it's possible that we
+      // have multiple instances of some managers. It's
+      // likely they communicate with the same simulation
+      // instance or may operate separately as long as no
+      // external script depends on calls to Instance.
+      // E.g., ContactMaterialManager and CollisionGroupsManager
+      // may have several instances. Simulation and
+      // ScriptAssetManager may not, so either they has
+      // to be in the main scene or in no scene at all.
+      if ( s_instance == null )
+        Instance = this as T;
     }
 
     protected override void OnDestroy()
     {
-      Debug.Log( $"OnDestroy: {typeof( T ).FullName}, {s_instance != null}" );
-      Instance = null;
+      if ( s_instance == this )
+        Instance = null;
 
       base.OnDestroy();
     }
 
+    /// <summary>
+    /// Invalid to call this method if s_instance != null. Finds
+    /// instance in scene or creates a new game object with component T.
+    /// </summary>
+    /// <param name="onlyFind">
+    /// True if only FindObjectOfType should be called and if not found,
+    /// return null. False to find or create a new instance.
+    /// </param>
+    /// <returns>
+    /// Found or new instance. Null if <paramref name="onlyFind"/> == true and
+    /// FindObjectOfType returns null.
+    /// </returns>
     private static T FindOrCreateInstance( bool onlyFind = false )
     {
       if ( s_instance != null )
         Debug.LogError( $"{s_instance.name}: Invalid to call FindOrCreateInstance called with non-null s_instance." );
 
-      if ( IsDestroyed )
-        Debug.LogWarning( $"{typeof( T ).FullName}: IsDestroyed == true and finding/creating a new instance." );
+      // It's important that the caller knows something in which context
+      // the Instance call is made in so that we're not recreating this
+      // object in, e.g., OnDestroy/Unloading of as scene.
+      s_wasCreated = false;
 
       Instance = FindObjectOfType<T>();
-      Debug.Log( $"Found in scene: {typeof( T ).FullName}, {s_instance != null}" );
-      if ( !onlyFind && s_instance == null ) {
+      if ( !onlyFind && s_instance == null )
         Instance = new GameObject( typeof( T ).FullName ).AddComponent<T>();
-        Debug.Log( $"Create new: {typeof( T ).FullName}, {s_instance != null}" );
-      }
 
       if ( s_instance != null ) {
         s_instance.transform.hideFlags = HideFlags.NotEditable;
@@ -173,13 +120,11 @@ namespace AGXUnity
 
     private static void OnSceneUnloaded( Scene scene )
     {
-      Debug.Log( "OnSceneUnloaded: " + scene.name );
       s_wasCreated = false;
       SceneManager.sceneUnloaded -= OnSceneUnloaded;
     }
 
     private static T s_instance = null;
     private static bool s_wasCreated = false;
-#endif
   }
 }
