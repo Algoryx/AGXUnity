cd13bea3044e28b25555d29d0553f60e9b8d98d1
Joakim Mörk <joakim.mork@algoryx.se>
2021-11-01 15:33:31 +0100

    Boilerplate for rest of the weight values

---
+++ Editor/CustomEditors/AGXUnity+CableDamageEditor.cs
@@ -0,0 +1,13 @@
+
+using System;
+using AGXUnity;
+using AGXUnity.Collide;
+using UnityEditor;
+
+namespace AGXUnityEditor.Editors
+{
+  [CustomEditor( typeof( AGXUnity.CableDamage ) )]
+  [CanEditMultipleObjects]
+  public class AGXUnityCableDamageEditor : InspectorEditor
+  { }
+}
