922d375a993ea1b020cad526cec9329e3b749ec7
Joakim Mörk <joakim.mork@algoryx.se>
2021-11-01 12:09:46 +0100

    Now uses ScriptableAsset for settings and properly propagates one of the properties

--- AGXUnity/CableDamage.cs
+++ AGXUnity/CableDamage.cs
@@ -5,6 +5,8 @@ using AGXUnity.Utils;
 
 namespace AGXUnity
 {
+  [DisallowMultipleComponent] // TODO not 100 that we want to disallow multiples?
+  [RequireComponent(typeof(AGXUnity.Cable))] // TODO other ScriptComponents does this through code instead of Attribute
   public class CableDamage : ScriptComponent
   {
     /// <summary>
@@ -12,8 +14,89 @@ namespace AGXUnity
     /// </summary>
     public agxCable.CableDamage Native { get; private set; }
 
+    /// <summary>
+    /// The Cable ScriptComponent that this CableDamage follows
+    /// </summary>
+    [HideInInspector]
+    public Cable Cable { get { return m_cable ?? ( m_cable = GetComponent<Cable>() ); } }
 
+    public void TestyTest()
+    {
+      var dam = new agxCable.CableDamage();
+      dam.setStretchDeformationWeight(1);
+    }
     //TODO
     //public void RestoreLocalDataFrom( agx.RigidBody native )
+
+
+    [SerializeField]
+    private CableDamageProperties m_properties = null;
+
+    [AllowRecursiveEditing]
+    public CableDamageProperties Properties
+    {
+      get { return m_properties; }
+      set
+      {
+        if ( Native != null && m_properties != null && m_properties != value )
+          m_properties.Unregister( this );
+
+        m_properties = value;
+
+        if ( Native != null && m_properties != null )
+          m_properties.Register( this );
+      }
+    }
+    
+    protected override bool Initialize()
+    {
+      Native = new agxCable.CableDamage();
+
+      // TODO remove this if we don't find any use for the native cable in Initialize()
+      // var cable = Cable?.GetInitialized<Cable>()?.Native;
+      // if ( cable == null ) {
+      //   Debug.LogWarning( "Unable to find Cable component for CableDamage - cable damage instance ignored.", this );
+      //   return false;
+      // }
+
+      if ( Properties == null ) {
+        Properties = ScriptAsset.Create<CableDamageProperties>();
+        Properties.name = "[Temporary] Cable Damage Properties";
+      }
+
+      return true;
+    }
+
+    void Update()
+    {
+      Debug.Log("Test: " + Properties.BendDeformation);
+    }
+
+    protected override void OnDestroy()
+    {
+      if ( Properties != null )
+        Properties.Unregister( this );
+
+      Native = null;
+
+      base.OnDestroy();
+    }
+
+    protected override void OnEnable()
+    {
+    }
+
+    protected override void OnDisable()
+    {
+    }
+
+    private void Reset()
+    {
+      if ( GetComponent<Cable>() == null )
+        Debug.LogError( "Component: CableDamage requires Cable component.", this );
+    }
+
+    private Cable m_cable = null;
+
   }
 }
