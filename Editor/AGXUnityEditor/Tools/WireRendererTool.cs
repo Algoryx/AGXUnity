48bc00bc5b7fe1c749fe3a4138a9c9cbab99e532
Joakim Mörk <joakim.mork@algoryx.se>
2021-12-28 10:57:22 +0100

    Multiselect works, hide params not relevant to render mode

--- Editor/AGXUnityEditor/Tools/WireRendererTool.cs
+++ Editor/AGXUnityEditor/Tools/WireRendererTool.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using GUI = AGXUnity.Utils.GUI;
+using System.Linq;
 
 using AGXUnity.Rendering;
 
@@ -25,22 +26,80 @@ namespace AGXUnityEditor.Tools
     public override void OnPreTargetMembersGUI()
     {
       var skin = InspectorEditor.Skin;
+      var WRTargets = GetTargets<WireRenderer>();
 
-      WireRenderer.Material = (Material)EditorGUILayout.ObjectField(GUI.MakeLabel("Render Material"), WireRenderer.Material, typeof(Material), true);
-      WireRenderer.RenderMode = (WireRenderer.SegmentRenderMode)EditorGUILayout.EnumPopup( 
+      bool differentModes = false;
+      if (IsMultiSelect)
+        foreach (var WR in WRTargets)
+          if (WR.RenderMode != WireRenderer.RenderMode)
+          {
+            differentModes = true;
+          }
+
+      EditorGUI.showMixedValue = WRTargets.Any( WR => !Equals( WireRenderer.Material, WR.Material ) );
+      var material = (Material)EditorGUILayout.ObjectField(GUI.MakeLabel("Render Material"), WireRenderer.Material, typeof(Material), true);
+      if ( UnityEngine.GUI.changed )
+      {
+        foreach (var WR in WRTargets)
+          WR.Material = material;
+        UnityEngine.GUI.changed = false;
+      }
+      EditorGUI.showMixedValue = false;
+
+      EditorGUI.showMixedValue = WRTargets.Any( WR => !Equals( WireRenderer.RenderMode, WR.RenderMode ) );
+      var renderMode = (WireRenderer.SegmentRenderMode)EditorGUILayout.EnumPopup( 
                                       GUI.MakeLabel( "Render Mode" ),
                                       WireRenderer.RenderMode,
                                       InspectorEditor.Skin.Popup );
-      using ( InspectorGUI.IndentScope.Single ) {
-        if (WireRenderer.RenderMode == WireRenderer.SegmentRenderMode.GameObject){
-          WireRenderer.NumberOfSegmentsPerMeter = EditorGUILayout.FloatField( GUI.MakeLabel("Segments per Meter"), WireRenderer.NumberOfSegmentsPerMeter );
+      if ( UnityEngine.GUI.changed )
+      {
+        foreach (var WR in WRTargets)
+          WR.RenderMode = renderMode;
+        UnityEngine.GUI.changed = false;
+      }
+      EditorGUI.showMixedValue = false;
+
+      using ( InspectorGUI.IndentScope.Single ) 
+      {
+        if (differentModes)
+          return;
+
+        if (WireRenderer.RenderMode == WireRenderer.SegmentRenderMode.GameObject)
+        {
+          EditorGUI.showMixedValue = WRTargets.Any( WR => !Equals( WireRenderer.NumberOfSegmentsPerMeter, WR.NumberOfSegmentsPerMeter ) );
+          var nrs = EditorGUILayout.FloatField( GUI.MakeLabel("Segments per Meter"), WireRenderer.NumberOfSegmentsPerMeter );
+          if ( UnityEngine.GUI.changed )
+          {
+            foreach (var WR in WRTargets)
+              WR.NumberOfSegmentsPerMeter = nrs;
+            UnityEngine.GUI.changed = false;
+          }
+          EditorGUI.showMixedValue = false;
         }
-        else {
-          WireRenderer.ShadowCastingMode = (UnityEngine.Rendering.ShadowCastingMode)EditorGUILayout.EnumPopup(
+        else 
+        {
+          EditorGUI.showMixedValue = WRTargets.Any( WR => !Equals( WireRenderer.ShadowCastingMode, WR.ShadowCastingMode ) );
+          var castShadows = (UnityEngine.Rendering.ShadowCastingMode)EditorGUILayout.EnumPopup(
                                               GUI.MakeLabel("Shadow Casting Mode"), 
                                               WireRenderer.ShadowCastingMode,
                                               InspectorEditor.Skin.Popup);
-          WireRenderer.ReceiveShadows = EditorGUILayout.Toggle(GUI.MakeLabel("Receive Shadows"), WireRenderer.ReceiveShadows );
+          if ( UnityEngine.GUI.changed )
+          {
+            foreach (var WR in WRTargets)
+              WR.ShadowCastingMode = castShadows;
+            UnityEngine.GUI.changed = false;
+          }
+          EditorGUI.showMixedValue = false;
+
+          EditorGUI.showMixedValue = WRTargets.Any( WR => !Equals( WireRenderer.ReceiveShadows, WR.ReceiveShadows ) );
+          var receiveShadows = EditorGUILayout.Toggle(GUI.MakeLabel("Receive Shadows"), WireRenderer.ReceiveShadows );
+          if ( UnityEngine.GUI.changed )
+          {
+            foreach (var WR in WRTargets)
+              WR.ReceiveShadows = receiveShadows;
+            UnityEngine.GUI.changed = false;
+          }
+          EditorGUI.showMixedValue = false;
         }
       }
     }
