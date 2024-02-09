7b811013d7f621cd87faca4ff97de98cb8d5dfdd
Filip Henningsson <filip.henningsson@algoryx.com>
2024-02-09 15:59:18 +0100

    Remove old shape visual verify as this is done in EditorUpdate method instead

--- Editor/AGXUnityEditor/AutoUpdateSceneHandler.cs
+++ Editor/AGXUnityEditor/AutoUpdateSceneHandler.cs
@@ -18,7 +18,6 @@ namespace AGXUnityEditor
         return;
 
       VerifyOnSelectionTarget( scene );
-      VerifyShapeVisualsMaterial( scene );
       VerifySimulationInstance( scene );
       VerifyCableWireRendering( scene );
       VerifyTerrainMaterials();
@@ -100,27 +99,6 @@ namespace AGXUnityEditor
           foreach ( var instance in data )
             Component.DestroyImmediate( instance );
           data = null;
-        }
-      }
-    }
-
-    /// <summary>
-    /// Shape visual components with sharedMaterial == null are assigned default material.
-    /// </summary>
-    private static void VerifyShapeVisualsMaterial( Scene scene )
-    {
-      var shapeVisuals = Object.FindObjectsOfType<AGXUnity.Rendering.ShapeVisual>();
-      foreach ( var shapeVisual in shapeVisuals ) {
-        var renderers = shapeVisual.GetComponentsInChildren<MeshRenderer>();
-        foreach ( var renderer in renderers ) {
-          if ( renderer.sharedMaterial == null ) {
-            renderer.sharedMaterial = Manager.GetOrCreateShapeVisualDefaultMaterial();
-
-            Debug.Log( "Shape visual with null material. Assigning default.", shapeVisual );
-
-            if ( !EditorApplication.isPlaying )
-              UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty( scene );
-          }
         }
       }
     }
