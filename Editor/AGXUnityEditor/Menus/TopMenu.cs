2e90c0f3d8245bbb6b084b819e15a87612b8d87f
Joakim Mörk <joakim.mork@algoryx.com>
2024-04-09 17:02:13 +0200

    Create AGX objects in middle of scene view camera instead of at origin

--- Editor/AGXUnityEditor/Menus/TopMenu.cs
+++ Editor/AGXUnityEditor/Menus/TopMenu.cs
@@ -3,6 +3,7 @@ using AGXUnity.Collide;
 using AGXUnity.Model;
 using AGXUnity.Utils;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using Mesh = AGXUnity.Collide.Mesh;
 using Plane = AGXUnity.Collide.Plane;
@@ -26,9 +27,16 @@ namespace AGXUnityEditor
         return null;
 
       var parent = command.context as GameObject;
+      var views = SceneView.sceneViews;
       if ( parent != null )
         go.transform.SetParent( parent.transform, false );
-
+      else if (SceneView.sceneViews.Count > 0)
+      {
+        var view = SceneView.sceneViews[0] as SceneView;
+        if (view != null)
+          view.MoveToView(go.transform);
+      }
+      
       AGXUnity.Rendering.ShapeVisual.Create( go.GetComponent<T>() );
 
       Undo.RegisterCreatedObjectUndo( go, "shape" );
