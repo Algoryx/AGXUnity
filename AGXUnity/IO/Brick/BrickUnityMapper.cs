4407cf3e3da067fb052e893a235967044c1f3b16
Filip Henningsson <filip.henningsson@algoryx.com>
2024-05-02 15:41:14 +0200

    Add shovels to terrains automatically

--- AGXUnity/IO/Brick/BrickUnityMapper.cs
+++ AGXUnity/IO/Brick/BrickUnityMapper.cs
@@ -450,6 +450,7 @@ namespace AGXUnity.IO.BrickIO
       mapped.CuttingEdge = Line.Create( body.gameObject, shovel.cutting_edge().start().ToHandedVector3(), shovel.cutting_edge().end().ToHandedVector3() );
       mapped.CuttingDirection = Line.Create( body.gameObject, Vector3.zero, shovel.cutting_direction().ToHandedVector3() );
       mapped.CuttingDirection.Start.LocalRotation = Quaternion.FromToRotation( Vector3.up, shovel.cutting_direction().ToHandedVector3() );
+      mapped.AutoAddToTerrains = true;
     }
 
     void mapSystemToCollisionGroup( Brick.Physics3D.System system, CollisionGroup collision_group )
