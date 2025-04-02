f27aac47cd1612f75eb15edf5f8d66f10293d946
Filip Henningsson <filip.henningsson@algoryx.com>
2025-04-02 16:04:11 +0200

    Added support for a clamshell shoovel

---
+++ AGXUnity/Model/ClamshellBucket.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace AGXUnity.Model
+{
+  public class ClamshellBucket : ScriptComponent
+  {
+    public agxTerrain.ClamShellBucket Native { get; private set; } = null;
+
+    [field: SerializeField]
+    public DeformableTerrainShovel Shovel1 { get; set; } = null;
+
+    [field: SerializeField]
+    public DeformableTerrainShovel Shovel2 { get; set; } = null;
+
+    [SerializeField]
+    private float m_closedThreshold = 0.1f;
+    public float ClosedThreshold
+    {
+      get => m_closedThreshold;
+      set
+      {
+        m_closedThreshold = value;
+        if ( Native != null )
+          Native.setClosedThreshold( value );
+      }
+    }
+
+    protected override bool Initialize()
+    {
+      var s1 = Shovel1?.GetInitialized<DeformableTerrainShovel>()?.Native;
+      var s2 = Shovel2?.GetInitialized<DeformableTerrainShovel>()?.Native;
+      if ( s1 == null || s2 == null ) {
+        Debug.LogError( "Failed to initialize one of the provided clamshell halves!" );
+        return false;
+      }
+
+      Native = new agxTerrain.ClamShellBucket( s1, s2 );
+      Simulation.Instance.Native.add( Native );
+      return true;
+    }
+  }
+}
