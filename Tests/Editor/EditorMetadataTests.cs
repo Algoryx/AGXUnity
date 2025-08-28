65cec77d95538b732ec0f25df9d9303771739dc9
Filip Henningsson <filip.henningsson@algoryx.com>
2025-08-28 14:55:48 +0200

    Add Editor test that all components have explicit AddComponent menu path

--- Tests/Editor/EditorMetadataTests.cs
+++ Tests/Editor/EditorMetadataTests.cs
@@ -39,6 +39,33 @@ namespace AGXUnityTesting.Editor
       }
     }
 
+
+    [Test]
+    public void ScriptsHaveExplicitAddComponentPath()
+    {
+      var asm = Assembly.GetAssembly(typeof(RigidBody));
+      List<Type> missing = new List<Type>();
+      foreach ( var t in asm.ExportedTypes ) {
+        if (
+          (
+            t.IsSubclassOf( typeof( ScriptComponent ) )
+          ) && !t.IsAbstract ) {
+          var help = t.GetCustomAttribute<AddComponentMenu>(false);
+          //var hide = t.GetCustomAttribute<HideInInspector>();
+          if ( help == null /*&& hide == null*/ )
+            missing.Add( t );
+        }
+      }
+
+      if ( missing.Count > 0 ) {
+        var errStr = $"The following classes are missing the AddComponent Attribute: \n";
+        foreach ( var t in missing ) {
+          errStr += t.Name + "\n";
+        }
+        Assert.Fail( errStr );
+      }
+    }
+
     private void CheckScriptIconsInDirectory( string path )
     {
       if ( System.IO.Directory.Exists( path ) ) {
