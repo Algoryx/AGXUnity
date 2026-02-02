79225badd6a26e4c48d9bced196c62cbd295edd7
Joakim Mörk <joakim.mork@algoryx.com>
2026-02-02 15:01:13 +0100

    Adjust test path

--- AGXUnity/Sensor/LidarSensor.cs
+++ AGXUnity/Sensor/LidarSensor.cs
@@ -14,7 +14,7 @@ namespace AGXUnity.Sensor
     static public bool LidarRayPatternsAreFound()
     {
       var _ = NativeHandler.Instance;
-      var fileTest = agxIO.Environment.instance().getFilePath( agxIO.Environment.Type.RESOURCE_PATH ).find( "MaterialLibrary/TerrainMaterials/avia.bin" );
+      var fileTest = agxIO.Environment.instance().getFilePath( agxIO.Environment.Type.RESOURCE_PATH ).find( "MaterialLibrary/LidarRayPatterns/avia.bin" );
       return !string.IsNullOrEmpty( fileTest );
     }
   }
