1f8438e0a0b6b2acf1306206d3167d9165206997
Joakim Mörk <joakim.mork@algoryx.com>
2026-06-04 15:02:19 +0200

    Profiling for terrain height change renderer

--- AGXUnity/Rendering/DebugTerrainHeightChangeRenderer.cs
+++ AGXUnity/Rendering/DebugTerrainHeightChangeRenderer.cs
@@ -3,6 +3,7 @@ using AGXUnity.Utils;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Profiling;
 
 namespace AGXUnity.Rendering
 {
@@ -26,34 +27,46 @@ namespace AGXUnity.Rendering
 
       public TerrainDebugData( Terrain terrain, TerrainLayer loweredLayer, TerrainLayer raisedLayer )
       {
-        Terrain = terrain;
-        TerrainData = terrain.terrainData;
-        InitialLayers = TerrainData.terrainLayers;
-        InitialLayerCount = InitialLayers.Length;
-        InitialAlphamaps = TerrainData.GetAlphamaps( 0, 0, TerrainData.alphamapWidth, TerrainData.alphamapHeight );
-        InitialHeights = TerrainData.GetHeights( 0, 0, TerrainData.heightmapResolution, TerrainData.heightmapResolution );
+        s_createTerrainDataSampler.Begin();
+        try {
+          Terrain = terrain;
+          TerrainData = terrain.terrainData;
+          InitialLayers = TerrainData.terrainLayers;
+          InitialLayerCount = InitialLayers.Length;
+          InitialAlphamaps = TerrainData.GetAlphamaps( 0, 0, TerrainData.alphamapWidth, TerrainData.alphamapHeight );
+          InitialHeights = TerrainData.GetHeights( 0, 0, TerrainData.heightmapResolution, TerrainData.heightmapResolution );
 
-        var layers = InitialLayers.ToList();
-        if ( !layers.Contains( loweredLayer ) )
-          layers.Add( loweredLayer );
-        LoweredLayerIndex = layers.IndexOf( loweredLayer );
+          var layers = InitialLayers.ToList();
+          if ( !layers.Contains( loweredLayer ) )
+            layers.Add( loweredLayer );
+          LoweredLayerIndex = layers.IndexOf( loweredLayer );
 
-        if ( !layers.Contains( raisedLayer ) )
-          layers.Add( raisedLayer );
-        RaisedLayerIndex = layers.IndexOf( raisedLayer );
+          if ( !layers.Contains( raisedLayer ) )
+            layers.Add( raisedLayer );
+          RaisedLayerIndex = layers.IndexOf( raisedLayer );
 
-        TerrainData.terrainLayers = layers.ToArray();
-        Alphamaps = TerrainData.GetAlphamaps( 0, 0, TerrainData.alphamapWidth, TerrainData.alphamapHeight );
-        RestoreAlphamapBuffer();
+          TerrainData.terrainLayers = layers.ToArray();
+          Alphamaps = TerrainData.GetAlphamaps( 0, 0, TerrainData.alphamapWidth, TerrainData.alphamapHeight );
+          RestoreAlphamapBuffer();
+        }
+        finally {
+          s_createTerrainDataSampler.End();
+        }
       }
 
       public void RestoreAlphamapBuffer()
       {
-        for ( int y = 0; y < TerrainData.alphamapHeight; y++ ) {
-          for ( int x = 0; x < TerrainData.alphamapWidth; x++ ) {
-            for ( int layer = 0; layer < Alphamaps.GetLength( 2 ); layer++ )
-              Alphamaps[ y, x, layer ] = layer < InitialLayerCount ? InitialAlphamaps[ y, x, layer ] : 0.0f;
+        s_restoreAlphamapBufferSampler.Begin();
+        try {
+          for ( int y = 0; y < TerrainData.alphamapHeight; y++ ) {
+            for ( int x = 0; x < TerrainData.alphamapWidth; x++ ) {
+              for ( int layer = 0; layer < Alphamaps.GetLength( 2 ); layer++ )
+                Alphamaps[ y, x, layer ] = layer < InitialLayerCount ? InitialAlphamaps[ y, x, layer ] : 0.0f;
+            }
           }
+        }
+        finally {
+          s_restoreAlphamapBufferSampler.End();
         }
       }
 
@@ -62,8 +75,14 @@ namespace AGXUnity.Rendering
         if ( TerrainData == null )
           return;
 
-        TerrainData.terrainLayers = InitialLayers;
-        TerrainData.SetAlphamaps( 0, 0, InitialAlphamaps );
+        s_resetTerrainDataSampler.Begin();
+        try {
+          TerrainData.terrainLayers = InitialLayers;
+          TerrainData.SetAlphamaps( 0, 0, InitialAlphamaps );
+        }
+        finally {
+          s_resetTerrainDataSampler.End();
+        }
       }
     }
 
@@ -71,6 +90,7 @@ namespace AGXUnity.Rendering
     private TerrainLayer m_loweredLayer = null;
 
     [IgnoreSynchronization]
+    [DisableInRuntimeInspector]
     public TerrainLayer LoweredLayer
     {
       get => m_loweredLayer;
@@ -81,6 +101,7 @@ namespace AGXUnity.Rendering
     private TerrainLayer m_raisedLayer = null;
 
     [IgnoreSynchronization]
+    [DisableInRuntimeInspector]
     public TerrainLayer RaisedLayer
     {
       get => m_raisedLayer;
@@ -88,12 +109,22 @@ namespace AGXUnity.Rendering
     }
 
     [field: SerializeField]
+    [DisableInRuntimeInspector]
     [Tooltip( "Fallback color used when Lowered Layer is not assigned." )]
     public Color LoweredColor { get; set; } = new Color( 0.85f, 0.18f, 0.08f, 1.0f );
 
     [field: SerializeField]
+    [DisableInRuntimeInspector]
     [Tooltip( "Fallback color used when Raised Layer is not assigned." )]
     public Color RaisedColor { get; set; } = new Color( 0.12f, 0.45f, 1.0f, 1.0f );
+
+    [field: SerializeField]
+    [Tooltip( "Whether lowered terrain should be painted." )]
+    public bool EnableLowered { get; set; } = true;
+
+    [field: SerializeField]
+    [Tooltip( "Whether raised terrain should be painted." )]
+    public bool EnableRaised { get; set; } = true;
 
     [field: SerializeField]
     [ClampAboveZeroInInspector( true )]
@@ -117,12 +148,18 @@ namespace AGXUnity.Rendering
 
     public void Clear()
     {
-      foreach ( var data in m_terrainData.Values ) {
-        data.RestoreAlphamapBuffer();
-        data.TerrainData.SetAlphamaps( 0, 0, data.Alphamaps );
-        data.IsDirty = false;
+      s_clearSampler.Begin();
+      try {
+        foreach ( var data in m_terrainData.Values ) {
+          data.RestoreAlphamapBuffer();
+          data.TerrainData.SetAlphamaps( 0, 0, data.Alphamaps );
+          data.IsDirty = false;
+        }
+        m_dirtyTerrains.Clear();
       }
-      m_dirtyTerrains.Clear();
+      finally {
+        s_clearSampler.End();
+      }
     }
 
     protected override bool Initialize()
@@ -282,77 +319,111 @@ namespace AGXUnity.Rendering
 
     private void PostStep()
     {
-      foreach ( var data in m_dirtyTerrains ) {
-        if ( data.TerrainData != null )
-          data.TerrainData.SetAlphamaps( 0, 0, data.Alphamaps );
-        data.IsDirty = false;
-      }
+      s_postStepSampler.Begin();
+      try {
+        foreach ( var data in m_dirtyTerrains ) {
+          if ( data.TerrainData != null ) {
+            s_setAlphamapsSampler.Begin();
+            try {
+              data.TerrainData.SetAlphamaps( 0, 0, data.Alphamaps );
+            }
+            finally {
+              s_setAlphamapsSampler.End();
+            }
+          }
+          data.IsDirty = false;
+        }
 
-      m_dirtyTerrains.Clear();
+        m_dirtyTerrains.Clear();
+      }
+      finally {
+        s_postStepSampler.End();
+      }
     }
 
     private void UpdateTextureAt( agxTerrain.Terrain aTerr, agx.Vec2i aIdx, Terrain uTerr, Vector2Int uIdx )
     {
-      var data = EnsureTerrainData( uTerr );
-      if ( data == null )
-        return;
+      s_updateTextureAtSampler.Begin();
+      try {
+        var data = EnsureTerrainData( uTerr );
+        if ( data == null )
+          return;
 
-      var td = data.TerrainData;
-      var baselineHeight = data.InitialHeights[ uIdx.y, uIdx.x ] * td.heightmapScale.y;
-      var currentHeight = td.GetHeight( uIdx.x, uIdx.y );
-      var delta = currentHeight - baselineHeight;
+        var td = data.TerrainData;
+        var baselineHeight = data.InitialHeights[ uIdx.y, uIdx.x ] * td.heightmapScale.y;
+        var currentHeight = td.GetHeight( uIdx.x, uIdx.y );
+        var delta = currentHeight - baselineHeight;
 
-      var debugLayer = -1;
-      var absDelta = Mathf.Abs( delta );
-      if ( delta < -HeightThreshold )
-        debugLayer = data.LoweredLayerIndex;
-      else if ( delta > HeightThreshold )
-        debugLayer = data.RaisedLayerIndex;
+        var debugLayer = -1;
+        var absDelta = Mathf.Abs( delta );
+        if ( EnableLowered && delta < -HeightThreshold )
+          debugLayer = data.LoweredLayerIndex;
+        else if ( EnableRaised && delta > HeightThreshold )
+          debugLayer = data.RaisedLayerIndex;
 
-      var fullStrengthHeightChange = Mathf.Max( FullStrengthHeightChange, HeightThreshold + Mathf.Epsilon );
-      var strength = debugLayer >= 0 ?
-                       Mathf.Clamp01( PaintStrength ) *
-                       Mathf.InverseLerp( HeightThreshold, fullStrengthHeightChange, absDelta ) :
-                       0.0f;
+        var fullStrengthHeightChange = Mathf.Max( FullStrengthHeightChange, HeightThreshold + Mathf.Epsilon );
+        var strength = debugLayer >= 0 ?
+                         Mathf.Clamp01( PaintStrength ) *
+                         Mathf.InverseLerp( HeightThreshold, fullStrengthHeightChange, absDelta ) :
+                         0.0f;
 
-      Paint( data, uIdx, debugLayer, strength );
+        Paint( data, uIdx, debugLayer, strength );
 
-      if ( !data.IsDirty ) {
-        data.IsDirty = true;
-        m_dirtyTerrains.Add( data );
+        if ( !data.IsDirty ) {
+          data.IsDirty = true;
+          m_dirtyTerrains.Add( data );
+        }
+      }
+      finally {
+        s_updateTextureAtSampler.End();
       }
     }
 
     private void Paint( TerrainDebugData data, Vector2Int heightIndex, int debugLayer, float strength )
     {
-      var td = data.TerrainData;
-      var alphamapWidth = td.alphamapWidth;
-      var alphamapHeight = td.alphamapHeight;
-      var heightmapMaxX = td.heightmapResolution - 1;
-      var heightmapMaxY = td.heightmapResolution - 1;
+      s_paintSampler.Begin();
+      try {
+        var td = data.TerrainData;
+        var alphamapWidth = td.alphamapWidth;
+        var alphamapHeight = td.alphamapHeight;
+        var heightmapMaxX = td.heightmapResolution - 1;
+        var heightmapMaxY = td.heightmapResolution - 1;
 
-      var minAlphaX = Mathf.FloorToInt( ( heightIndex.x - 0.5f - BrushRadius ) / heightmapMaxX * alphamapWidth );
-      var minAlphaY = Mathf.FloorToInt( ( heightIndex.y - 0.5f - BrushRadius ) / heightmapMaxY * alphamapHeight );
-      var maxAlphaX = Mathf.CeilToInt( ( heightIndex.x + 0.5f + BrushRadius ) / heightmapMaxX * alphamapWidth );
-      var maxAlphaY = Mathf.CeilToInt( ( heightIndex.y + 0.5f + BrushRadius ) / heightmapMaxY * alphamapHeight );
+        var minAlphaX = Mathf.FloorToInt( ( heightIndex.x - 0.5f - BrushRadius ) / heightmapMaxX * alphamapWidth );
+        var minAlphaY = Mathf.FloorToInt( ( heightIndex.y - 0.5f - BrushRadius ) / heightmapMaxY * alphamapHeight );
+        var maxAlphaX = Mathf.CeilToInt( ( heightIndex.x + 0.5f + BrushRadius ) / heightmapMaxX * alphamapWidth );
+        var maxAlphaY = Mathf.CeilToInt( ( heightIndex.y + 0.5f + BrushRadius ) / heightmapMaxY * alphamapHeight );
 
-      minAlphaX = Mathf.Clamp( minAlphaX, 0, alphamapWidth );
-      minAlphaY = Mathf.Clamp( minAlphaY, 0, alphamapHeight );
-      maxAlphaX = Mathf.Clamp( Mathf.Max( maxAlphaX, minAlphaX + 1 ), 0, alphamapWidth );
-      maxAlphaY = Mathf.Clamp( Mathf.Max( maxAlphaY, minAlphaY + 1 ), 0, alphamapHeight );
+        minAlphaX = Mathf.Clamp( minAlphaX, 0, alphamapWidth );
+        minAlphaY = Mathf.Clamp( minAlphaY, 0, alphamapHeight );
+        maxAlphaX = Mathf.Clamp( Mathf.Max( maxAlphaX, minAlphaX + 1 ), 0, alphamapWidth );
+        maxAlphaY = Mathf.Clamp( Mathf.Max( maxAlphaY, minAlphaY + 1 ), 0, alphamapHeight );
 
-      for ( int y = minAlphaY; y < maxAlphaY; y++ ) {
-        for ( int x = minAlphaX; x < maxAlphaX; x++ ) {
-          for ( int layer = 0; layer < data.Alphamaps.GetLength( 2 ); layer++ ) {
-            var original = layer < data.InitialLayerCount ? data.InitialAlphamaps[ y, x, layer ] : 0.0f;
-            data.Alphamaps[ y, x, layer ] = original * ( 1.0f - strength );
+        for ( int y = minAlphaY; y < maxAlphaY; y++ ) {
+          for ( int x = minAlphaX; x < maxAlphaX; x++ ) {
+            for ( int layer = 0; layer < data.Alphamaps.GetLength( 2 ); layer++ ) {
+              var original = layer < data.InitialLayerCount ? data.InitialAlphamaps[ y, x, layer ] : 0.0f;
+              data.Alphamaps[ y, x, layer ] = original * ( 1.0f - strength );
+            }
+
+            if ( debugLayer >= 0 )
+              data.Alphamaps[ y, x, debugLayer ] += strength;
           }
-
-          if ( debugLayer >= 0 )
-            data.Alphamaps[ y, x, debugLayer ] += strength;
         }
       }
+      finally {
+        s_paintSampler.End();
+      }
     }
+
+    private static readonly CustomSampler s_createTerrainDataSampler = CustomSampler.Create( "DebugTerrainHeightChangeRenderer.CreateTerrainData" );
+    private static readonly CustomSampler s_restoreAlphamapBufferSampler = CustomSampler.Create( "DebugTerrainHeightChangeRenderer.RestoreAlphamapBuffer" );
+    private static readonly CustomSampler s_resetTerrainDataSampler = CustomSampler.Create( "DebugTerrainHeightChangeRenderer.ResetTerrainData" );
+    private static readonly CustomSampler s_clearSampler = CustomSampler.Create( "DebugTerrainHeightChangeRenderer.Clear" );
+    private static readonly CustomSampler s_postStepSampler = CustomSampler.Create( "DebugTerrainHeightChangeRenderer.PostStep" );
+    private static readonly CustomSampler s_setAlphamapsSampler = CustomSampler.Create( "DebugTerrainHeightChangeRenderer.SetAlphamaps" );
+    private static readonly CustomSampler s_updateTextureAtSampler = CustomSampler.Create( "DebugTerrainHeightChangeRenderer.UpdateTextureAt" );
+    private static readonly CustomSampler s_paintSampler = CustomSampler.Create( "DebugTerrainHeightChangeRenderer.Paint" );
 
     private readonly Dictionary<Terrain, TerrainDebugData> m_terrainData = new Dictionary<Terrain, TerrainDebugData>();
     private readonly List<TerrainDebugData> m_dirtyTerrains = new List<TerrainDebugData>();
