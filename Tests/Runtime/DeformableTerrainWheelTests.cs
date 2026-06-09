aa508c1205883ff9397b6528eb269e6b808740a4
Joakim Mörk <joakim.mork@algoryx.com>
2026-06-09 16:23:56 +0200

    Add tests with terrain contacts

--- Tests/Runtime/DeformableTerrainWheelTests.cs
+++ Tests/Runtime/DeformableTerrainWheelTests.cs
@@ -10,6 +10,10 @@ namespace AGXUnityTesting.Runtime
 {
   public class DeformableTerrainWheelTests : AGXUnityFixture
   {
+    private const float WheelRadius = 0.2f;
+    private const float WheelHeight = 0.2f;
+    private const float WheelClearance = 0.05f;
+
     [UnityTest]
     public IEnumerator CreateAndDestroyWithoutCylinder()
     {
@@ -42,6 +46,81 @@ namespace AGXUnityTesting.Runtime
       yield return TestUtils.DestroyAndWait( go );
 
       Assert.That( wheel.Native, Is.Null );
+    }
+
+    [UnityTest]
+    public IEnumerator WheelWithoutMaterialsDoesntWork()
+    {
+      var (terrain, wheel) = CreateTerrainAndWheel();
+
+      TestUtils.InitializeAll();
+
+      for ( int i = 0; i < 20; ++i )
+        yield return TestUtils.Step();
+
+      Assert.That( terrain.Native, Is.Not.Null );
+      Assert.That( wheel.Native, Is.Not.Null );
+      Assert.That( wheel.ActiveContactMaterialUsesTerrainWheelForceModel, Is.False );
+    }
+
+    [UnityTest]
+    public IEnumerator WheelWithWrongFrictionModel()
+    {
+      var (terrain, wheel) = CreateTerrainWheelAndContactMaterial();
+
+      TestUtils.InitializeAll();
+
+      for ( int i = 0; i < 20; ++i )
+        yield return TestUtils.Step();
+
+      Assert.That( terrain.Native, Is.Not.Null );
+      Assert.That( wheel.Native, Is.Not.Null );
+      Assert.That( wheel.ActiveContactMaterialUsesTerrainWheelForceModel, Is.False );
+    }
+
+    private (DeformableTerrain terrain, DeformableTerrainWheel wheel) CreateTerrainAndWheel()
+    {
+      var terrainGO = new GameObject( "Deformable Terrain" );
+      var unityTerrain = terrainGO.AddComponent<Terrain>();
+      unityTerrain.terrainData = new TerrainData();
+      unityTerrain.terrainData.size = new Vector3( 2.0f, 1.0f, 2.0f );
+      unityTerrain.terrainData.heightmapResolution = 33;
+
+      var terrain = terrainGO.AddComponent<DeformableTerrain>();
+      terrain.MaximumDepth = 0.5f;
+
+      var cylinderGO = Factory.Create<Cylinder>();
+      var cylinder = cylinderGO.GetComponent<Cylinder>();
+      cylinder.Radius = WheelRadius;
+      cylinder.Height = WheelHeight;
+      cylinder.Material = ShapeMaterial.CreateInstance<ShapeMaterial>();
+
+      var wheelGO = Factory.Create<RigidBody>( cylinderGO );
+      wheelGO.transform.position = new Vector3( 1.0f, WheelRadius + WheelClearance, 1.0f );
+      wheelGO.transform.rotation = Quaternion.Euler( 90.0f, 0.0f, 0.0f );
+
+      var wheel = wheelGO.AddComponent<DeformableTerrainWheel>();
+
+      return (terrain, wheel);
+    }
+
+    private (DeformableTerrain terrain, DeformableTerrainWheel wheel) CreateTerrainWheelAndContactMaterial()
+    {
+      var (terrain, wheel) = CreateTerrainAndWheel();
+
+      var wheelMaterial = ShapeMaterial.CreateInstance<ShapeMaterial>();
+      var terrainMaterial = ShapeMaterial.CreateInstance<ShapeMaterial>();
+
+      wheel.RigidBody.GetComponentInChildren<Cylinder>().Material = wheelMaterial;
+      terrain.Material = terrainMaterial;
+
+      var contactMaterial = ContactMaterial.CreateInstance<ContactMaterial>();
+      contactMaterial.Material1 = wheelMaterial;
+      contactMaterial.Material2 = terrainMaterial;
+
+      ContactMaterialManager.Instance.Add( contactMaterial );
+
+      return (terrain, wheel);
     }
   }
 }
